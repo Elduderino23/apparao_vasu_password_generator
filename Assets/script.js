80c187ed793716178cbc31fcd69c919af9ddcb0f
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-09-28 22:39:55 -0700

    fix script.js to make password box

--- Assets/script.js
+++ Assets/script.js
@@ -4,12 +4,24 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var general = [];
   let passwordLength = prompt("How many character do you want? More than 8 but less than 128.");
+  if (passwordLength == null || passwordLength == "") {
+    alert("Error, follow the instruction");
+    passwordLength = "you canceled"
+    return passwordLength;
+
+  } else if (parseInt(passwordLength) < 8) {
+    return passwordLength = "Too small"
+
+  } else if (parseInt(passwordLength) > 128) {
+    return passwordLength = "Too big"
+
+  }
   var askSpecial = false;
   if (confirm("Do You Want Special Characters!")) {
     askSpecial = true;
-    // console.log(askSpecial);
+
     general.push(symbols);
-    // console.log(general)
+
   }
   var askUppercase = false;
   if (confirm("Do You Want Uppercase Characters")) {
@@ -27,9 +39,8 @@ function generatePassword() {
     general.push(numbers);
     askNumerical = true;
 
+
   }
-  // var index = Math.floor(Math.random() * general.length);
-  // var index2 = Math.floor(Math.random() * general[index].length);
   console.log(general)
   let pw = "";
   console.log(pw);
@@ -47,7 +58,7 @@ function generatePassword() {
 // Write password to the #password input
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "+", "_", "?", "~", "(", ")", ";", ",",]
+var symbols = [" ", "!", "”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", ",", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\ ", "]", "^", "_", "`", "{", "|", "}", "~",]
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
 
 function writePassword() {
@@ -55,22 +66,14 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
-  if (confirm) {
+  let writtenPassword = alert("Here is your password")
+  if (writtenPassword = password) {
+    return alert(password);
 
   }
-}
-// var lowerCase = ["a"]
-// if(works){
 
-// }
-// if (password === 1) {
-//   console.log("character 1");
-// } else if (passwordText === "password") {
-//   console.log("password generated");
-// } else if (passwordText === "Nada") {
-//   console.log("Wrong Password, Please Try Again");
-// } else {
-//   console.log("Your So Wrong, You Ain't Right");
+}
+
 
 
 
